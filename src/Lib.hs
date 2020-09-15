a0d7d34a85ff390a922c0d0731d632c3742c67bb
Ilya Alonov <fatawesomeee@gmail.com>
2020-09-15 21:34:34 +0300

    [fix] forgot that VSCode doesnt save on type by default

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,49 +1,66 @@
-{-# LANGUAGE GADTs #-}
-
 module Lib
-  ( someFunc,
+  ( showTest,
   )
 where
 
 import Data.Monoid
 import Data.Semigroup
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Interval t e = Interval (t, t) e deriving Show
 
-data Interval t e = Interval (t, t) e
+newtype Timeline t e = Timeline [Interval t e] deriving Show
 
-newtype Timeline t e = Timeline [Interval t e]
+-- эксперименты 
+-- newtype TimelineWithoutConflicts t e = TimelineWithoutConflicts (Timeline t e)
 
-instance Semigroup (Timeline t e) where
-  Timeline x <> Timeline y = Timeline (x <> y)
+-- instance Semigroup (TimelineWithoutConflicts t e) where
+--   TimelineWithoutConflicts left <> TimelineWithoutConflicts right = TimelineWithoutConflicts (t1 <> t2)
 
-instance Monoid (Timeline t e) where
-  mempty = Timeline []
-
-orderIntervals ::
-  Ord t =>
-  Interval t e ->
-  Interval t e ->
-  Maybe ((Interval t e), (Interval t e))
-orderIntervals (startLeft, endLeft) payloadLeft (startRight, endRight) payloadRight
-  | (startLeft < startRight) && (endLeft < endRight) =
-    Just (Interval ((startLeft, endLeft) payloadLeft) Interval ((startRight, endRight) payloadRight))
+-- instance Semigroup (Timeline t e) where
+--   Timeline x <> Timeline y = mconcat result
+--     where
+--       result :: [TimelineWithoutConflicts t e]
+--       result = resolveConflicts x y
 
 mergeIntervals ::
   Ord t =>
   (Interval t e -> Interval t e -> Interval t e) ->
   Interval t e ->
   Interval t e ->
-  Interval t e
-mergeIntervals resolveConflict i1 i2 =
-  _
+  Timeline t e
+mergeIntervals resolveConflict interval1 interval2
+  | (start1 < start2) && (end1 < end2) =
+    Timeline [interval1, interval2]
+  | (start1 > start2) && (end1 > end2) =
+    Timeline [interval2, interval1]
+  | otherwise = Timeline [resolveConflict interval1 interval2]
+  where
+    Interval (start1, end1) _ = interval1
+    Interval (start2, end2) _ = interval2
 
+-- / реализация работает только если timeline1 и timeline2 не содержат в себе конфликтов.
+-- / TODO: функция может вернуть таймлайн с конфликтами
 concatTimelines ::
   Ord t =>
-  Timeline t e ->
-  Timeline t e ->
   (Interval t e -> Interval t e -> Interval t e) ->
+  Timeline t e ->
+  Timeline t e ->
   Timeline t e
-concatTimelines (Timeline intervals1) (Timeline intervals2) resolveConflict =
-  Timeline (zipWith (mergeIntervals resolveConflict) intervals1 intervals2)
+concatTimelines resolvePayloadConflict timeline1 timeline2 
+  = Timeline (concatMap (\ (Timeline i) -> i) timelineList)
+  where
+    Timeline intervals1 = timeline1
+    Timeline intervals2 = timeline2
+    timelineList = zipWith (mergeIntervals resolvePayloadConflict) intervals1 intervals2
+
+testResolveConflict :: Interval t e -> Interval t e -> Interval t e
+testResolveConflict left _ = left
+
+test :: Timeline Integer [Char]
+test = concatTimelines testResolveConflict timeline1 timeline2
+  where
+    timeline1 = Timeline [Interval (1, 2) "a1", Interval (4, 5) "b1"]
+    timeline2 = Timeline [Interval (1, 3) "a2", Interval (4, 6) "b2"]
+
+showTest :: IO ()
+showTest = print test

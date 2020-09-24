cce316b9599a6f626d991e79286030f00850c41c
Ilya Alonov <fatawesomeee@gmail.com>
2020-09-24 18:55:52 +0300

    [wip] insert to timeline

--- src/Timeline.hs
+++ src/Timeline.hs
@@ -7,6 +7,8 @@ import           Data.Semigroup
 import           Interval
 import           OverlappingTimeline
 
+import           Data.List           (sort)
+
 -- | Timeline cannot have overlapping intervals.
 --
 -- prop> not (haveConflicts (toList t))
@@ -23,8 +25,8 @@ concatIntervalsWithPayloads
   -> (Interval t, e)
   -> [(Interval t, e)]
 concatIntervalsWithPayloads
-  mergePayload 
-  pair1@(Interval (left1, right1), e1) 
+  mergePayload
+  pair1@(Interval (left1, right1), e1)
   pair2@(Interval (left2, right2), e2)
   | right1 > left2 =
     [ (Interval (left1, left2), e1)
@@ -32,6 +34,35 @@ concatIntervalsWithPayloads
     , (Interval (right1, right2), e2)
     ]
   | otherwise = [pair1, pair2]
+
+-- LATEST 2 FUNCTIONS
+-- !!!WIP!!! warning
+-- | Resolve conflict so that the result is in the bounds of lesser interval
+resolveConflictToLeft
+  :: Ord t
+  => (e -> e -> e)
+  -> (Interval t, e)
+  -> (Interval t, e)
+  -> [(Interval t, e)]
+resolveConflictToLeft resolve (i1, e1) (i2, e2)
+  | end1 < start2 = [(Interval (start1, end1), e1)]
+  | otherwise =
+    [ (Interval (start1, start2), e1)
+    , (Interval (start2, end1), resolve e1 e2)
+    ]
+  where
+    [left, right] = sort [i1, i2]
+    Interval (start1, end1) = left
+    Interval (start2, end2) = right
+
+insert
+  :: Ord t
+  => (e -> e -> e)
+  -> (Interval t, e)
+  -> Timeline t e
+  -> Timeline t e
+insert mergePayload elem (Timeline xs)
+  = Timeline (foldl (<>) [] (map (resolveConflictToLeft mergePayload elem) xs))
 
 fromOverlappingTimeline
   :: Ord t
@@ -41,7 +72,7 @@ fromOverlappingTimeline
 fromOverlappingTimeline mergePayload (OverlappingTimeline xs) = Timeline (resolveConflicts xs)
   where
     resolveConflicts [] = []
-    resolveConflicts (t:ts)  = foldM (concatIntervalsWithPayloads mergePayload) t ts
+    resolveConflicts (t:ts) = foldM (concatIntervalsWithPayloads mergePayload) t ts
     -- ^ f [ ((1, 4), 'a'), ((2, 5), 'b') ] = [ ((1, 2), 'a'), ((2, 4), 'ab'), ((4, 5), 'ab') ]
     -- foldM (?)
 
@@ -55,16 +86,20 @@ fromListWith
   -> Timeline t e      -- ^ new Timeline
 fromListWith f lst = fromOverlappingTimeline f (fromList lst)
 
--- mergeTimeline
---   :: Ord t
---   => (e -> e -> e)
---   -> Timeline t e
---   -> Timeline t e
--- mergeTimeline f timeline = _
+mergeTimeline
+  :: Ord t
+  => (e -> e -> e)
+  -> Timeline t e
+  -> Timeline t e
+  -> Timeline t e
+mergeTimeline f (Timeline xs) (Timeline ys) = fromListWith f (xs <> ys)
 
 emptyTimeline :: Timeline t e
 emptyTimeline = Timeline []
 
 -- | just for testing purposes
-res = fromOverlappingTimeline (++) (OverlappingTimeline [(Interval (1, 4), "a"), (Interval (2, 5), "b")])
+res = fromOverlappingTimeline (++) (OverlappingTimeline [(Interval (1, 4), "a"), (Interval (2, 5), "b"), (Interval (6, 9), "b")])
 res2 = fromListWith (++) [(Interval (2, 5), "b"), (Interval (1, 4), "a")]
+res3 = mergeTimeline (++) (Timeline [(Interval (1, 4), "a"), (Interval (5, 7), "b")]) (Timeline [(Interval (2, 6), "a"), (Interval (7, 9), "b")])
+
+res5 = insert (++) (Interval (2, 6), "a") (Timeline [(Interval (1, 4), "a"), (Interval (5, 7), "b")])

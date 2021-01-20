974ef657fc3a17d2b9bcc8fcd79ba7a51802b34d
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2021-01-20 10:21:32 +0300

    Tiny doctest fix

--- src/Event.hs
+++ src/Event.hs
@@ -1,25 +1,28 @@
 module Event where
 
-import Interval
+import           Interval
 
 data Event t p = Event {
   interval :: Interval t,
-  payload :: p
+  payload  :: p
 } deriving (Show, Eq)
 
 -- | /O(1)/.
 --
--- prop> fromTuple (Interval (1,2), 'a') == Event (Interval (1,2)) 'a' 
+-- prop> fromTuple (Interval (1,2), 'a') == Event (Interval (1,2)) 'a'
 fromTuple :: (Interval t, p) -> Event t p
 fromTuple (i, p) = Event i p
 
 -- | /O(1)/. Events are adjancent if one strictly follows other and their payloads are equal.
 --
--- prop> Event.adjacent (Event (Interval (1,2)) 'a') (Event (Interval (2,3)) 'a') == True
+-- >>> e1 = Event (Interval (1,2)) 'a'
+-- >>> e2 = Event (Interval (2,3)) 'a'
+-- >>> e1 `adjancent` e2
+-- True
 adjacent
-  :: (Ord t, Eq p) 
+  :: (Ord t, Eq p)
   => Event t p
   -> Event t p
   -> Bool
-adjacent e1 e2 
+adjacent e1 e2
   = Interval.adjacent (interval e1) (interval e2) && payload e1 == payload e2

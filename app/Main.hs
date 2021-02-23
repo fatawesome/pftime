263502567bc634ac58bcc1ebad6d6e1f17809063
Ilya Alonov <fatawesomeee@gmail.com>
2021-02-23 20:06:26 +0300

    add

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+import Visuals.Relations as Relations
+import Visuals.Ascending as Ascending
+import Visuals.NoConflicts as NoConflicts
+import Visuals.Timeline as Timeline
+
 main :: IO ()
-main = print 'a'
+main = Timeline.unionByPic
